58b5665ed07e5a8274aed9a63dc94bfca8e8d087
bianchengxiaobei <443850741@qq.com>
2017-03-17 10:36:37 +0800

    新增加一些项目必要的文件，和开始场景的搭建

---
+++ DigitalBeastExplorer/Assets/Scripts/Driver.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+/************
+*日期：2017.3.17
+*作者：chenfuling
+*功能：游戏驱动器
+*************/
+public class Driver : MonoBehaviour 
+{
+    private static Driver m_sInstance;
+
+    void Awake()
+    {
+        m_sInstance = this;
+        Application.targetFrameRate = 60;
+    }
+	void Start ()
+	{
+	
+	}
+
+	void Update () 
+	{
+	
+	}
+}
