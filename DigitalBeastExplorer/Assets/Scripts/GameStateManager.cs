43d6a765ea57547c6bc5c6d7c430bf981bcd4dcd
bianchengxiaobei <443850741@qq.com>
2017-05-09 17:44:37 +0800

    新增了框架

--- DigitalBeastExplorer/Assets/Scripts/GameStateManager.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GameStateManager
-{
-    public static GameStateManager m_oInstance;
-}
---
+++ DigitalBeastExplorer/Assets/Scripts/GameStates/LoginState.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using UnityEngine;
+using CaomaoFramework;
+
+public class LoginState : ClientStateBase
+{
+    public override void OnEnter()
+    {
+        
+    }
+    public override void OnLeave()
+    {
+        
+    }
+}
